ba5db4e9bcc53725ff07de9e97657bf784c1983a
Violet-Riah <violettanjombwa@gmail.com>
2024-03-22 19:54:37 +0300

    add file

---
+++ index.js
@@ -0,0 +1,37 @@
+let arr1=[3,7,34,90,12];
+console.log('last item', arr1[arr1.length-1]);
+
+
+let arr2=[true,"green","where",12,156];
+console.log('last item',arr1[arr1.length-1]);
+
+const pet =["Caw","Bird","Snake","Dog"];
+let mypet = pet.toString();
+console.log(mypet);
+
+var item = [-5,9,5,3,2,-3,6,8,4,1];
+console.log(arr3.sort(function(a, b){return a -b}));
+console.log(item);
+
+
+let arr = ("boy","man","girl","school","girl","woman");
+function filterArray(arr){
+    return arr. filter((arr,
+        index arr) => arr.indexOf(item)
+}
+console.log(filterArray(arr))
+
+
+
+
+
+
+
+
+
+
+let word = "renniw";
+console.log("split", word.split(1));
+
+
+let prineeds=()
